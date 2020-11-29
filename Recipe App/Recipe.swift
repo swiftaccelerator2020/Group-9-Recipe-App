2c21e9db1d18cd1cc9b33f72cf43726b73350c1a
LimMengShin <lim_meng_shin@s2019.ssts.edu.sg>
2020-11-29 14:33:33 +0800

    Edited Recipe structs (doesn't work yet)

--- Recipe App/Recipe.swift
+++ Recipe App/Recipe.swift
@@ -14,15 +14,83 @@ struct RecipesInfo: Decodable {
     let image: String?
     let id: Int?
     let readyInMinutes: Int?
+    let servings: Int?
     let aggregateLikes: Int?
+    let sourceName: String?
+    let sourceUrl: String?
     let summary: String?
+    let nutrition: [RecipeNutrition]?
     let analyzedInstructions: [RecipeInstructions]?
+    
+//    var equipments: [RecipeStepEquipment] {
+//      var equipmentsArray: [RecipeStepEquipment] = []
+//      if let analyzedInstructions = analyzedInstructions {
+//        for analyzedInstruction in analyzedInstructions {
+//          if let steps = analyzedInstruction.steps {
+//            for step in steps {
+//              if let equipment = step.equipment {
+//                equipmentsArray += equipment
+//              }
+//            }
+//          }
+//        }
+//      }
+//      return equipmentsArray.unique()
+//    }
+
 }
 
+// Nutrition and Ingredients
+struct RecipeNutrition: Decodable {
+    let nutrients: [RecipeNutrients]?
+    let ingredients: [RecipeIngredients]?
+}
+
+struct RecipeNutrients: Decodable {
+    let title: String?
+    let amount: Double?
+    let unit: String?
+    let percentOfDailyNeeds: Double?
+}
+
+struct RecipeIngredients: Decodable {
+    let name: String?
+    let amount: Double?
+    let unit: String?
+}
+
+// Instructions
 struct RecipeInstructions: Decodable {
     let steps: [RecipeSteps]?
 }
 
 struct RecipeSteps: Decodable {
     let step: String?
+    let ingredients: [RecipeStepIngredients]?
+    let equipment: [RecipeStepEquipment]?
+    let length: [RecipeStepLength]?
+}
+
+struct RecipeStepIngredients: Decodable {
+    let id: Int?
+    let name: String?
+    let image: String?
+}
+
+struct RecipeStepEquipment: Decodable, Hashable {
+    let id: Int?
+    let name: String?
+    let image: String?
+}
+
+struct RecipeStepLength: Decodable {
+    let number: Int?
+    let unit: String?
+}
+
+extension Sequence where Iterator.Element: Hashable {
+    func unique() -> [Iterator.Element] {
+        var seen: Set<Iterator.Element> = []
+        return filter { seen.insert($0).inserted }
+    }
 }

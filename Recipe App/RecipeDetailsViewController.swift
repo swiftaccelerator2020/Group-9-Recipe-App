fc03a365b7b969d36b723ee07cbe4de84b249801
LimMengShin <lim_meng_shin@s2019.ssts.edu.sg>
2020-11-30 12:32:52 +0800

    Fixed minor AutoLayout problems, standardised button heights, and recipeImageView in RecipeDetailsViewController has corner radius and a shadow now

--- Recipe App/RecipeDetailsViewController.swift
+++ Recipe App/RecipeDetailsViewController.swift
@@ -10,20 +10,21 @@ import UIKit
 class RecipeDetailsViewController: UIViewController {
     
     var recipes: RecipesInfo?
-    @IBOutlet weak var recipeImageView: UIImageView!
     
+    @IBOutlet weak var recipeImageView: UIImageView!
     @IBOutlet weak var recipeUIView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for view in [recipeImageView, recipeUIView] {
-            view!.layer.shadowColor = UIColor.gray.cgColor
-            view!.layer.shadowOpacity = 1
-            view!.layer.shadowOffset = CGSize(width: 1, height: 1)
-            view!.layer.cornerRadius = 15
-            view!.clipsToBounds = false
-        }
+        recipeUIView.layer.shadowColor = UIColor.gray.cgColor
+        recipeUIView.layer.shadowOpacity = 1
+        recipeUIView.layer.shadowOffset = CGSize(width: 1, height: 1)
+        recipeUIView.layer.cornerRadius = 15
+        recipeUIView.clipsToBounds = false
+        
+        recipeImageView.layer.cornerRadius = 15
+        recipeImageView.clipsToBounds = true
         
         
         self.recipeImageView.downloaded(from: recipes?.image ?? "https://i.stack.imgur.com/Vkq2a.png")
@@ -31,19 +32,6 @@ class RecipeDetailsViewController: UIViewController {
             if let title = recipes.title {
                 self.title = title
             }
-            
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
